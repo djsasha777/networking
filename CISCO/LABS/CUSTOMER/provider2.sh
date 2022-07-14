7bed5748256f32ffc93908b81248450ff014d5ea
Alex <djsasha777@mail.ru>
2022-07-14 11:32:22 +0300

    up

--- CISCO/LABS/CUSTOMER/provider2.sh
+++ CISCO/LABS/CUSTOMER/provider2.sh
@@ -1,35 +1,32 @@
 conf t
 
-hostname PROVIDER1
+hostname PROVIDER2
 
 interface Ethernet0/0
- ip address 192.168.1.30 255.255.255.0
+ ip address 192.168.1.31 255.255.255.0
  ip nat outside
  ip virtual-reassembly in
  no shutdown
 
 interface Ethernet1/0
- ip address 174.1.0.1 255.255.255.0
+ ip address 175.1.0.1 255.255.255.0
  ip nat inside
  ip virtual-reassembly in
  no shutdown
 
 interface Ethernet1/1
- ip address 174.2.0.1 255.255.255.0
+ ip address 175.2.0.1 255.255.255.0
  ip nat inside
  ip virtual-reassembly in
  no shutdown
 
- exit
-
-router bgp 3890
+router bgp 65783
  bgp log-neighbor-changes
  network 0.0.0.0
- neighbor 174.1.0.2 remote-as 56789
- neighbor 174.1.0.2 password MYPASS
- neighbor 174.2.0.2 remote-as 56789
- neighbor 174.2.0.2 password MYPASS
-
+ neighbor 175.1.0.2 remote-as 56789
+ neighbor 175.1.0.2 password MYPASS
+ neighbor 175.2.0.2 remote-as 56789
+ neighbor 175.2.0.2 password MYPASS
 
  exit
 
