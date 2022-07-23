98f56832d55cb7ad417fb501681a5ad19b5e32f6
Alex <djsasha777@mail.ru>
2022-07-23 14:33:26 +0300

    up

--- CISCO/LABS/CUSTOMER/ALT-core2.sh
+++ CISCO/LABS/CUSTOMER/ALT-core2.sh
@@ -2,9 +2,6 @@ conf t
 
 hostname CORE2
 
-interface Loopback0
- ip address 10.0.0.6 255.255.255.255
-
 interface Ethernet0/0
  ip address 172.0.0.2 255.255.255.0
  no shutdown
@@ -25,24 +22,14 @@ interface Ethernet1/1
  ip address 172.0.5.1 255.255.255.0
  no shutdown
 
+ exit
+
 router ospf 1
- network 10.0.0.0 0.0.0.255 area 1
  network 172.0.0.0 0.0.0.255 area 1
  network 172.0.3.0 0.0.0.255 area 1
  network 172.0.5.0 0.0.0.255 area 1
  network 173.2.0.0 0.0.0.255 area 1
  network 173.4.0.0 0.0.0.255 area 1
- default-information originate
-
-router bgp 56789
- bgp router-id 10.0.0.6
- bgp log-neighbor-changes
- neighbor 10.0.0.3 remote-as 56789
- neighbor 10.0.0.3 update-source Loopback0
- neighbor 10.0.0.4 remote-as 56789
- neighbor 10.0.0.4 update-source Loopback0
- neighbor 10.0.0.5 remote-as 56789
- neighbor 10.0.0.5 update-source Loopback0
  default-information originate
 
 end
